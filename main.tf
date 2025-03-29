f0d18a38176d419ebefacfd380a8905e90870a21
Venom-143 <sushant14satkar@gmail.com>
2025-03-29 18:21:35 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    grafana = {
+      source = "grafana/grafana"
+      version = "~> 1.11.3"  # Optional: specify the version you want
+    }
+  }
+}
+
 provider "grafana" {
   url   = var.grafana_url          # URL of the Grafana instance (e.g., http://localhost:3000)
   auth  = var.grafana_api_key      # API key to authenticate with Grafana
