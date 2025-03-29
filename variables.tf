e34cb79f1c83b15118c403adf97e67e2f489fdde
Venom-143 <sushant14satkar@gmail.com>
2025-03-29 18:00:13 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "grafana_url" {
+  description = "https://bolalag327.grafana.net/"
+  type        = string
+}
+
+variable "grafana_api_key" {
+  description = "glsa_sqAdiGhYdt0JGpe51c2DpS5flhjpD41C_4732185b"
+  type        = string
+}
+
+variable "prometheus_url" {
+  description = "https://prometheus-us-central1.grafana.net/api/prom"
+  type        = string
+}
